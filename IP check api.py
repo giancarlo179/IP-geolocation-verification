7800bfe10f68b11829829e5bce7bc02d6fd1df75
giancarlo179 <32601403+giancarlo179@users.noreply.github.com>
2022-03-18 22:23:34 -0400

    IP por argumento de entrada

--- IP check api.py
+++ IP check api.py
@@ -1,10 +1,13 @@
 import http.client
+import sys
 
+ip = (sys.argv[1])
+#ip = '8.8.8.8'
 #Utilizamos la API de la pagina https://ip-api.com/docs/api:json
 #Variable de conexion HTTP
 conn = http.client.HTTPConnection("ip-api.com", "80")
 #Enviamos la IP por medio del metodo GET
-conn.request("GET", "/json/191.95.56.25")
+conn.request("GET", "/json/%s" %ip)
 
 #Leemos la respuesta
 res = conn.getresponse()
